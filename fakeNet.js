b5748c48fd508f7574355593644259122c75cc2b
antimony123 <Benten@refracta>
2019-05-30 21:15:15 +0000

    Extend net simulation capabilities

--- fakeNet.js
+++ fakeNet.js
@@ -1,16 +1,52 @@
 "use strict";
 
-// Simple "broadcast" setup to enable easy testing
+/**
+ * Simulates a network by using events to enable simpler testing.
+ */
+module.exports = class FakeNet {
+    
+    constructor() {
+        this.clients = {};
+        this.clientCtr = 0;
+    }
+    
+    /**
+     * Registers clients to the network.
+     * Clients receive a random identity (e.g. client0, client1...)
+     * Miners are registered by name.
+     * In the future, may change this method to register by public key regardless of miner/client.
+     * 
+     * @param {...Object} clientlist - clients to be registered to this network (may be Client or Miner)
+     */
+    register(...clientlist) {
+        clientlist.forEach(client => {
+            let name = client.name || "client" + this.clientCtr++;
+            this.clients[name] = client;
+        });
+    }
 
-let clients = [];
+    /**
+     * Broadcasts to all clients within this.clients the message msg and payload o.
+     * 
+     * @param {String} msg - the name of the event being broadcasted (e.g. "PROOF_FOUND")
+     * @param {Objejct} o - payload of the message
+     */
+    broadcast(msg, o) {
+        Object.keys(this.clients).forEach((clientName) => {
+            this.sendMessage(clientName, msg, o);
+        });
+    }
 
-exports.register = function(...miners) {
-  miners.forEach(client => clients.push(client));
+    /**
+     * Sends message msg and payload o directly to Client name.
+     * 
+     * @param {String} name - the name of the client or miner to which to send the message
+     * @param {String} msg - the name of the event being broadcasted (e.g. "PROOF_FOUND")
+     * @param {Objejct} o - payload of the message
+     */
+    sendMessage(name, msg, o) {
+        let client = this.clients[name];
+        client.emit(msg, o);
+    }
+
 }
-
-exports.broadcast = function(msg, o) {
-  clients.forEach((client) => {
-    client.emit(msg, o);
-  });
-}
-
